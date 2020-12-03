39dce3a4992a767240760484a0451d031c6cd2f3
Simon Tenggren <simon.tenggren@gmail.com>
2020-12-03 15:43:47 +0100

    fixed some changes

--- Main.java
+++ Main.java
@@ -6,17 +6,16 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Missing input parameters!");
-            System.out.println("Usage: java Main <pattern> <file>");
+            System.out.println("Usage: java Main <pattern> <file-path>");
             System.exit(1);
         }
 
         Searcher s = new Searcher(args[0], args[1]);
         try {
-
-        List<String> matchedLines = s.search();
-        System.out.println("Matched lines in file " + args[1] + ":");
-        for (String l : matchedLines) 
-            System.out.println("\t" + l);
+            List<String> matchedLines = s.search();
+            System.out.println("Matched lines in file " + args[1] + ":");
+            for (String l : matchedLines) 
+                System.out.println("\t" + l);
         } catch (FileNotFoundException e) {
             System.err.println("File could not be found!");
             System.exit(1); 
